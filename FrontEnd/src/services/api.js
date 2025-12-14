7ac14edd93b85d3314aa408285f4f0e95fb4e33d
Akhil <akhil9381@outlook.com>
2025-12-15 01:13:07 +0530

    Configure frontend for production backend

--- FrontEnd/src/services/api.js
+++ FrontEnd/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://resume-builder-oopi.onrender.com",
   withCredentials: false,
 });
 api.interceptors.request.use((config) => {
